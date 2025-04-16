fd8aed92273b1ec8bcd06a122784e1d9b8d5970c
Kartonowy <ttrollface.com.pl@gmail.com>
2025-04-16 08:06:20 +0200

    context menus

---
+++ front/src/components/storage/ContextButton.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+import { ref } from 'vue';
+import ContextWindow from './ContextWindow.vue';
+import type { Position } from '../../utils/types';
+
+const props = defineProps<{
+    position: Position,
+    sleeve_key: PropertyKey | null | undefined
+}>();
+
+const isContextActive = ref(false);
+const position = ref({
+    x: 0,
+    y: 0
+})
+
+</script>
+
+<template>
+    <button @click="() => {isContextActive = !isContextActive}">+</button>
+    <ContextWindow :is-context-active="isContextActive" :position="position" :sleeve_key="sleeve_key"/>
+</template>
+
+<style scoped>
+    button {
+        width: 20%;
+        height: 100%;
+        border-radius: 25px;
+    }
+</style>
---
+++ front/src/components/storage/ContextWindow.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+import type { Position } from '../../utils/types';
+import { useGlobalState } from '../state';
+
+
+const props = defineProps<{
+    position: Position,
+    isContextActive: boolean,
+    sleeve_key: PropertyKey | null | undefined
+    // handler: () => boolean,
+}>();
+
+const positionD = {
+    left: 'calc(250px - 1.4rem)',
+    top: 'calc(60px - 1.65rem)' 
+}
+
+type TileBindable = [
+    name: string | number,
+    bind: () => void
+];
+
+const {items} = useGlobalState()
+
+const tiles: TileBindable[] = [
+    ["open", () => {
+        let ouritem = items.find((e) => e.sleevekey == props.sleeve_key)
+        window.open(ouritem?.url, '_blank')
+    }],
+    ["edit", () => {}],
+    ["tag", () => {}],
+    ["categories", () => {}],
+    ["highlight", () => {}],
+    ["delete", () => {}]
+];
+
+</script>
+
+<template>
+    <div :class="{ active: isContextActive }" class="container" :style="positionD" >
+        <div class="context-menu">
+            <div class="tile" v-for="tile in tiles">
+                <div @click="tile[1]">{{ tile[0] }}</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .container {
+        position: absolute;
+    }
+
+    :not(.active).container {
+        display: none;
+    }
+
+    .context-menu {
+        outline: 2px red solid;
+    }
+
+    .tile:hover {
+        background: red;
+    }
+</style>
