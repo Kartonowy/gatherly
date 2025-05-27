fc78300824c9722b27aa20406fb56ca752a6b752
Kartonowy <ttrollface.com.pl@gmail.com>
2025-05-27 14:45:36 +0200

    Changed to javascript, types are chore in elysia

--- back/src/routes/router.js
+++ back/src/routes/router.js
@@ -1,9 +1,10 @@
-import {Elysia, t} from "elysia";
+import {Elysia, error, t} from "elysia";
 
 import {createInsertSchema} from "drizzle-typebox";
 import {boardsTable, sleeveTable, usersTable} from "../db/schema.js";
 import {db} from "../db/index.js";
-import {eq} from "drizzle-orm";
+import {eq, or} from "drizzle-orm";
+import * as argon2 from "argon2";
 
 
 
@@ -29,9 +30,17 @@ const _createSleeve = createInsertSchema(sleeveTable, {
 })
 
 
+
 export const APIrouter = new Elysia({ prefix: "/api" })
-    .get('/', (req: any, res: any) => { console.log(req, res) })
-    .post("/sign-up", ({ body }: { body: object}) => {
+    .get('/', (req, res) => { console.log(req, res) })
+    .post("/sign-up", ({ body }) => {
+
+        const data = await db.select().from(usersTable).where(
+            or(
+                eq(usersTable.email, body.email),
+                eq(usersTable.name, body.username)
+            )
+        )
         // second VALIDATION OF CREDENTIALS
         // ADDING TO DATABASE
         // LOGGING IN OD RAZU (JWT)
@@ -42,12 +51,36 @@ export const APIrouter = new Elysia({ prefix: "/api" })
             ["id"]
         )
     })
-    .post("/log-in", ({ body }: { body: object}) => {
-        // CHECK CREDENTIALS
-        // ADD JWT
-        console.log(body)
+    .post("/log-in", async ({ body, jwt, cookie: { auth } }) => {
+
+        const user  = await db.select().from(usersTable).where(
+            or(
+                eq(usersTable.email, body.name),
+                eq(usersTable.name, body.name)
+            )
+        );
+        if (user.length > 1) {
+            return error(500)
+        }
+
+        if (user.length === 1) {
+            return error(404, "User not found")
+        }
+
+        if (await argon2.verify(user[0].password, body.password)) {
+            auth.set({
+                value: await jwt.sign(body.name),
+                httpOnly: true,
+                maxAge: 86400,
+                path: '/'
+            })
+
+            return `Sign in as ${auth.value}`
+        } else {
+            return error(401, "Invalid credentials")
+        }
     })
-    .post("/add-board", async ({ body }: any) => {
+    .post("/add-board", async ({ body }) => {
         // CHECK IF SUCCEED?
         console.log(await db.insert(boardsTable).values([{...body}]))
     }, {
@@ -56,11 +89,11 @@ export const APIrouter = new Elysia({ prefix: "/api" })
             ["id"]
         )
     })
-    .post("/board/", async ({body, error}: any) => {
+    .post("/board/", async ({body, error}) => {
             // Not adding dual eq check for sakes of future, might add collaborations meaning we have to check
             // both owner and collaborants
             const q = await db.select().from(boardsTable).where(eq(boardsTable.id, Number(body.id)));
-            if (q[0].owner == Number(body.owner)) {
+            if (q[0].owner === Number(body.owner)) {
                 return q
             }
             return error(403, "Forbidden")
@@ -71,14 +104,14 @@ export const APIrouter = new Elysia({ prefix: "/api" })
                 id: t.String()
             })
         })
-    .post("/sleeve", async ({body} : any) => {
+    .post("/sleeve", async ({body}) => {
         return db.select().from(sleeveTable).where(eq(sleeveTable.board, Number(body.board_id)));
     }, {
         body: t.Object({
             board_id: t.String()
         })
     })
-    .post("/add-sleeve", async ({body}: any) => {
+    .post("/add-sleeve", async ({body}) => {
         console.log(await db.insert(sleeveTable).values([{...body}]))
     },
         {
